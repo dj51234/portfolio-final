6fc487da2c163c56d26989a84d88faee593c3f2a
dj51234 <dj51234@yahoo.com>
2021-12-07 01:18:47 -0500

    folder reorganization

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,34 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass')(require('sass'));
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass")(require("sass"));
 
-gulp.task('serve', function () {
+gulp.task("serve", function () {
+  browserSync.init({
+    server: "./",
+  });
 
-    browserSync.init({
-        server: "./app"
-    });
-
-    gulp.watch("app/scss/*.scss", gulp.series('buildCSS'));
-    gulp.watch("app/scripts/*.js", gulp.series('scripts'));
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-
+  gulp.watch("./scss/*.scss", gulp.series("buildCSS"));
+  gulp.watch("./scripts/*.js", gulp.series("scripts"));
+  gulp.watch("./*.html").on("change", browserSync.reload);
 });
 
-gulp.task('buildCSS', function () {
-
-    return gulp.src(['app/scss/*.scss']).
-        pipe(sass({
-            includePaths: ['node_modules']
-          })).
-        pipe(gulp.dest("app/dest/styles")).
-        pipe(browserSync.stream());
-
+gulp.task("buildCSS", function () {
+  return gulp
+    .src(["./scss/*.scss"])
+    .pipe(
+      sass({
+        includePaths: ["node_modules"],
+      })
+    )
+    .pipe(gulp.dest("./dest/styles"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function () {
-
-    return gulp.src("app/scripts/**/*.js").
-        pipe(gulp.dest("app/dest/script")).
-        pipe(browserSync.stream());
-
+gulp.task("scripts", function () {
+  return gulp
+    .src("./scripts/**/*.js")
+    .pipe(gulp.dest("./dest/script"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('start', gulp.series('serve', 'buildCSS', 'scripts'));
+gulp.task("start", gulp.series("serve", "buildCSS", "scripts"));
